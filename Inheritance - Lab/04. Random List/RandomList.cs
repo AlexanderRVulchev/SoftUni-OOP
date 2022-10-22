86d4404a365b5e76129683a3bb6847ac4ec4fd4b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-22 13:45:08 +0300

    Add files via upload

---
+++ Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(this.Count);
+            string removed = this[index];
+            this.RemoveAt(index);
+            return removed;
+        }
+    }
+}
---
+++ Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,18 @@
+﻿//NOTE: You need a public StartUp class with the namespace CustomRandomList.
+//Create a RandomList class that has all the functionality of List<string>.
+//Add an additional function that returns and removes a random element from the list.
+//•	Public method: RandomString(): string
+
+
+using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
