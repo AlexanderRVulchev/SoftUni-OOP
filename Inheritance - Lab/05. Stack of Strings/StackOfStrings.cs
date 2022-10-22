e52fddd91f022030e5f63af910080b035f238fa9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-22 13:45:30 +0300

    Add files via upload

---
+++ Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+            => this.Count == 0;
+
+        public void AddRange(IEnumerable<string> collection)
+        {
+            foreach (string item in collection)
+                this.Push(item);
+        }
+    }
+}
---
+++ Inheritance - Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,19 @@
+﻿//NOTE: You need a public StartUp class with the namespace CustomStack.
+//Create a class StackOfStrings that extends Stack, can store only strings, and has the following functionality:
+//•	Public method: IsEmpty() : bool
+//•	Public method: AddRange() : Stack<string>
+
+
+using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+          
+        }
+    }
+}
