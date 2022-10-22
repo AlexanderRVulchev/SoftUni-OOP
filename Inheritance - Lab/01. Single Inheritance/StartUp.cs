41cb95c129102b8527342810f07d94ee3d2bf43e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-22 10:47:04 +0300

    Add files via upload

---
+++ Inheritance - Lab/01. Single Inheritance/StartUp.cs
@@ -0,0 +1,22 @@
+﻿//1.Single Inheritance
+//NOTE: You need a public StartUp class with the namespace Farm.
+//Create two classes named Animal and Dog:
+//•	Animal with a single public method Eat() that prints: "eating…"
+//•	Dog with a single public method Bark() that prints: "barking…"
+//•	Dog should inherit from Animal
+
+
+using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
