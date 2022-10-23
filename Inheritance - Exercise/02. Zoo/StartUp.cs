6bb2d8b702eee77b1e4cf9e70dbe1f7cd93b2121
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-23 09:12:06 +0300

    Add files via upload

---
+++ Inheritance - Exercise/02. Zoo/StartUp.cs
@@ -0,0 +1,21 @@
+﻿//NOTE: You need a public class StartUp.
+//Create a project Zoo. It needs to contain the following classes:  
+//Follow the diagram and create all of the classes.
+//Each of them, except the Animal class, should inherit from another class. Every class should have:
+//•	A constructor, which accepts one parameter: name.
+//•	Property Name - string.
+//Zip your solution without the bin and obj folders and upload it in Judge.
+
+
+using System;
+
+namespace Zoo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
