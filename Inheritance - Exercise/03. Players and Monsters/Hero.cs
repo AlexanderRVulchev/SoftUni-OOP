e39e46f23e81081777a10d941575cbf366f657de
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-23 09:12:39 +0300

    Add files via upload

---
+++ Inheritance - Exercise/03. Players and Monsters/Hero.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private string username;
+        private int level;
+
+        public string Username { get { return username; } set { username = value; } }
+        public int Level { get { return level; } set { level = value; } }
+
+        public Hero(string username, int level)
+        {
+            this.username = username;
+            this.level = level;
+        }
+
+        public override string ToString()
+            => $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+    }
+
+    public class Elf : Hero
+    {
+        public Elf(string username, int level) : base(username, level)
+        { }
+    }
+
+    public class Wizard : Hero
+    {
+        public Wizard(string username, int level) : base(username, level)
+        { }
+    }
+
+    public class Knight : Hero
+    {
+        public Knight(string username, int level) : base(username, level)
+        { }
+    }
+
+    public class MuseElf : Elf
+    {
+        public MuseElf(string username, int level) : base(username, level)
+        { }
+    }
+
+    public class DarkWizard : Wizard
+    {
+        public DarkWizard(string username, int level) : base(username, level)
+        { }
+    }
+
+    public class DarkKnight : Knight
+    {
+        public DarkKnight(string username, int level) : base(username, level)
+        { }
+    }
+
+    public class SoulMaster : DarkWizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        { }
+    }
+
+    public class BladeKnight : DarkKnight
+    {
+        public BladeKnight(string username, int level) : base(username, level)
+        { }
+    }
+}
---
+++ Inheritance - Exercise/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,23 @@
+﻿//NOTE: You need a public class StartUp.
+//Your task is to create the following game hierarchy: 
+// (Diagram missing here)
+//Create a class Hero. It should contain the following members:
+//•	A constructor, which accepts:
+//o username – string
+//o	level – int
+//•	The following properties:
+//o Username - string
+//o	Level – int
+//•	ToString() method
+
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
