9f5c7088dd9c7b9ff41468baa694e188522b51f0
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-23 09:11:50 +0300

    Add files via upload

---
+++ Inheritance - Exercise/02. Zoo/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo
+{
+    public class Animal
+    {
+        protected string name;
+
+        public string Name { get => this.name; set => this.name = value; }
+
+        public Animal(string name)
+        {
+            this.name = name;
+        }
+    }
+}
