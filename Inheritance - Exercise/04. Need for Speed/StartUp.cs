610b31ca659da5f8713ff1cfe86cfd5ee9288409
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-24 15:27:53 +0300

    Add files via upload

---
+++ Inheritance - Exercise/04. Need for Speed/StartUp.cs
@@ -0,0 +1,26 @@
+﻿//NOTE: You need a public class StartUp. Create the following hierarchy with the following classes:  
+
+//Create a base class Vehicle. It should contain the following members:
+//•	A constructor that accepts the following parameters: int horsePower, double fuel
+//•	DefaultFuelConsumption – double 
+//•	FuelConsumption – virtual double
+//•	Fuel – double
+//•	HorsePower – int
+//•	virtual void Drive(double kilometers)
+//o The Drive method should have a functionality to reduce the Fuel based on the traveled kilometers.
+//The default fuel consumption for Vehicle is 1.25. Some of the classes have different default fuel consumption values:
+//•	SportCar – DefaultFuelConsumption = 10
+//•	RaceMotorcycle – DefaultFuelConsumption = 8
+//•	Car – DefaultFuelConsumption = 3
+
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
---
+++ Inheritance - Exercise/04. Need for Speed/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public double DefaultFuelConsumption { get; set; }
+        public virtual double FuelConsumption { get { return this.DefaultFuelConsumption; } }
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+            this.DefaultFuelConsumption = 1.25;
+        }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * this.FuelConsumption;
+        }
+    }
+}
