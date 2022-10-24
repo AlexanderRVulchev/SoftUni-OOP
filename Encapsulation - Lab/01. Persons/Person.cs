8838055b203c66c4926cb45998c3ffe093efee78
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-24 15:34:04 +0300

    Add files via upload

---
+++ Encapsulation - Lab/01. Persons/Person.cs
@@ -0,0 +1,28 @@
+﻿
+namespace PersonsInfo
+{
+    public class Person
+    {
+        // Fields
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        // Properties
+        public string FirstName { get { return firstName; } private set { firstName = value; } }
+        public string LastName { get { return lastName; } private set { lastName = value; } }
+        public int Age { get { return age; } private set { age = value; } }
+
+        // Constructors
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        // Methods
+        public override string ToString()
+        => $"{this.firstName} {this.lastName} is {this.age} years old.";
+    }
+}
---
+++ Encapsulation - Lab/01. Persons/StartUp.cs
@@ -0,0 +1,19 @@
+﻿//NOTE: You need a public StartUp class with the namespace PersonsInfo.
+//Create a class Person, which should have public properties with private setters for:
+//•	FirstName: string
+//•	LastName: string
+//•	Age: int
+//•	ToString() : string - override
+
+using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
