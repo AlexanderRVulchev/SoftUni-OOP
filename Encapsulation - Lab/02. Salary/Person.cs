9a5d01272c70f40b21da41ab9778c3195ab08c63
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-24 15:34:16 +0300

    Add files via upload

---
+++ Encapsulation - Lab/02. Salary/Person.cs
@@ -0,0 +1,39 @@
+﻿
+namespace PersonsInfo
+{
+    public class Person
+    {
+        // Fields
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;        
+
+        // Properties
+        public string FirstName { get { return firstName; } private set { firstName = value; } }
+        public string LastName { get { return lastName; } private set { lastName = value; } }
+        public int Age { get { return age; } private set { age = value; } }
+        public decimal Salary { get { return salary; } private set { salary = value; } }        
+
+        // Constructors
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        // Methods
+        public override string ToString()
+        => $"{this.firstName} {this.lastName} receives {this.salary:f2} leva.";
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.age > 30)
+                this.salary += this.salary * percentage / 100;
+            else
+                this.salary += this.salary * percentage / 200;
+        }
+    }
+}
---
+++ Encapsulation - Lab/02. Salary/StartUp.cs
@@ -0,0 +1,18 @@
+﻿//Create objects of the class Person. Read their name, age, and salary from the console.
+//Read the percentage of the bonus to every Person's salary.
+//People younger than 30 get half the increase. Expand Person from the previous task.
+//New properties and methods:
+//•	Salary: decimal
+//•	IncreaseSalary(decimal percentage)
+
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
