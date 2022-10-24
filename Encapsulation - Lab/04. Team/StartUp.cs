04ebb7e7f0182e33d4f25607cc7c3511669c4c47
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-24 15:34:44 +0300

    Add files via upload

---
+++ Encapsulation - Lab/04. Team/StartUp.cs
@@ -0,0 +1,32 @@
+﻿//NOTE: You need a public StartUp class with the namespace PersonsInfo.
+//The skeleton from the previous task could be used.
+//Create a Team class. Add to this team all of the people you have received.
+//Those who are younger than 40 go to the first team, others go to the reserve team.
+//At the end print the sizes of the first and the reserved team.
+//The class should have private fields for:
+//•	name: string
+//•	firstTeam: List < Person >
+//•	reserveTeam: List < Person >
+//The class should have constructors:
+//•	Team(string name)
+//The class should also have public properties for:
+//•	FirstTeam: List<Person>(read - only!)
+//•	ReserveTeam: List<Person>(read - only!)
+//And a method for adding players:
+//•	AddPlayer(Person person): void
+
+
+
+using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ Encapsulation - Lab/04. Team/Team.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        // Fields
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        // Properties
+        public string Name { get { return name; } set { name = value; } }
+        public IReadOnlyCollection<Person> FirstTeam { get { return firstTeam.AsReadOnly(); } }
+        public IReadOnlyCollection<Person> ReserveTeam { get { return reserveTeam.AsReadOnly(); } }
+
+        // Constructors
+        public Team(string name)
+        {
+            this.Name = name;
+            this.firstTeam = new List<Person>();
+            this.reserveTeam = new List<Person>();
+        }
+
+        // Methods        
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+                this.firstTeam.Add(person);
+            else
+                this.reserveTeam.Add(person);
+        }
+    }
+}
