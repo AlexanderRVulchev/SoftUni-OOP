043d5174d07b4523d0163e1114dddb6cca4c2b8c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-24 15:27:19 +0300

    Add files via upload

---
+++ Inheritance - Exercise/04. Need for Speed/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        public Car(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+            this.DefaultFuelConsumption = 3;
+        }
+    }
+}
---
+++ Inheritance - Exercise/04. Need for Speed/FamilyCar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel)
+           : base(horsePower, fuel)
+        { }
+    }
+}
