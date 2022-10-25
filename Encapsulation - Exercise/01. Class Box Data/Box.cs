3ff42f5bbd00348adeb78f4fc0d5418eeecdce4f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-25 21:06:13 +0300

    Add files via upload

---
+++ Encapsulation - Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+
+namespace _01._Class_Box_Data
+{
+    public class Box
+    {
+        // Fields
+        private double length;
+        private double width;
+        private double height;
+
+        // Properties        
+        public double Length
+        {
+            get { return length; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                height = value;
+            }
+        }
+
+        // Constructors
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        // Methods
+        public double SurfaceArea()
+        => 2 * (length * height + width * height + width * length);
+
+        public double LateralSurfaceArea()
+        => 2 * (width * height + height * length);
+
+        public double Volume()
+        => length * width * height;
+
+        public override string ToString()
+        => $"Surface Area - {SurfaceArea():f2}" + Environment.NewLine +
+           $"Lateral Surface Area - {LateralSurfaceArea():f2}" + Environment.NewLine +
+           $"Volume - {Volume():f2}";
+    }
+}
+
---
+++ Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,46 @@
+﻿//Create a class Box, which has the following properties:
+//•	Length - double, should not be zero or negative number
+//•	Width - double, should not be zero or negative number
+//•	Height - double, should not be zero or negative number
+//If one of the properties is a zero or negative number throw ArgumentException with the message:
+//"{propertyName} cannot be zero or negative."
+//Use try-catch block to process the error.
+//All properties are set by the constructor and when set, they cannot be modified.
+//Behavior
+//double SurfaceArea()
+//Calculate and return the surface area of the Box.
+//double LateralSurfaceArea()
+//Calculate and return the lateral surface area of the Box.
+//double Volume()
+//Calculate and return the volume of the Box.
+//NOTE: You can find all formulas here.
+//Input
+//•	On the first three lines, you will get the length, width, and height. 
+//Output
+//•	On the next three lines print the surface area,
+//lateral surface area, and the volume of the box:
+
+
+using System;
+
+namespace _01._Class_Box_Data
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine(box);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
