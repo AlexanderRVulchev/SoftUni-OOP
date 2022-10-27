c43dff3856baac89198c0069e9395c0bf43694b3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-10-27 22:24:33 +0300

    Add files via upload

---
+++ Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    internal class Citizen : IPerson
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Citizen(string name, int age)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+    }
+}
