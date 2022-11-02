7ad241190a7dc46a3d300e1ac247241d595a11ce
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-02 18:10:33 +0200

    Add files via upload

---
+++ Exceptions and Error Handling - Lab/01. Square Root/Program.cs
@@ -0,0 +1,32 @@
+﻿//Write a program that reads an integer number and calculates and prints its square root. 
+//•	If the number is negative, print "Invalid number."
+//•	In all cases finally, print "Goodbye."
+//Use try-catch-finally.
+
+using System;
+
+namespace _01._Square_Root
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            try
+            {
+                if (num < 0)
+                    throw new ArgumentException("Invalid number.");
+                else
+                    Console.WriteLine(Math.Sqrt(num));
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
