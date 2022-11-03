db12c67d7038d7f69f0f3cba1ed9f61983470218
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-03 22:16:21 +0200

    Add files via upload

---
+++ Reflection and Attributes - Lab/03. Mission Private Impossible/Spy.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType);
+            foreach (FieldInfo field in fields)
+            {
+                if (requestedFields.Contains(field.Name))
+                {
+                    sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+            MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] nonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+
+            Object instance = Activator.CreateInstance(classType);
+
+            foreach (FieldInfo field in fields)
+                sb.AppendLine($"{field.Name} must be private!");
+
+            foreach (MethodInfo method in publicMethods.Where(m => m.Name.StartsWith("set")))
+                sb.AppendLine($"{method.Name} have to be private!");
+
+            foreach (MethodInfo method in nonPublicMethods.Where(m => m.Name.StartsWith("get")))
+                sb.AppendLine($"{method.Name} have to be public!");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string RevealPrivateMethods(string className)
+        {
+            StringBuilder sb = new StringBuilder();
+            Type classType = Type.GetType(className);
+            MethodInfo[] methodInfos = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            sb.AppendLine($"All Private Methods of Class: {classType.FullName}");
+            sb.AppendLine($"Base Class: {classType.BaseType.Name}");
+            foreach (MethodInfo method in methodInfos)
+                sb.AppendLine(method.Name);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/03. Mission Private Impossible/StartUp.cs
@@ -0,0 +1,28 @@
+﻿//NOTE: You need a public StartUp class with the namespace Stealer.
+//It’s time to see what this hacker you are dealing with aims to do.
+//Create a method inside your Spy class called - RevealPrivateMethods(string className).
+//Print all private methods in the following format:
+//All Private Methods of Class: {className
+//}
+//Base Class: { baseClassName}
+//On the next lines, print found method’s names each on a new line.
+//Use StringBuilder to concatenate the answer.
+//Don’t change anything in Hacker class!In your Main() method,
+//you should be able to check your program with the current piece of code.
+
+
+
+using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
