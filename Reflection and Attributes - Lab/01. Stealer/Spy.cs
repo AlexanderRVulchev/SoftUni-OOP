b5474d6dbeb52f4930c271e23a2cbf85d3b7fb9a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-03 22:15:41 +0200

    Add files via upload

---
+++ Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType);
+            foreach (FieldInfo field in fields)
+            {
+                if (requestedFields.Contains(field.Name))
+                {
+                    sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+                }
+            }                        
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/01. Stealer/StartUp.cs
@@ -0,0 +1,30 @@
+﻿//NOTE: You need a public StartUp class with the namespace Stealer.
+//Add the Hacker class from the box below to your project.
+
+//There is the one nasty hacker, but not so wise though.
+//He is trying to steal a big amount of money and transfer it to his account.
+//The police are after him but they need a professional… Correct - this is you!
+//You have the information that this hacker is keeping some of his info in private fields.
+//Create a new class named Spy and add inside a method called - StealFieldInfo, which receives:
+//•	string – the name of the class to investigate
+//•	an array of string - names of the fields to investigate
+//After finding the fields, you must print on the console:
+//"Class under investigation: {nameOfTheClass}"
+//On the next lines, print info about each field in the following format:
+//"{filedName} = {fieldValue}"
+//Use StringBuilder to concatenate the answer.Don’t change anything in Hacker class!
+
+using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            Console.WriteLine(result);
+        }
+    }
+}
