826eb9910034b8390dc4cf4ddc36df878eae185d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-03 22:17:34 +0200

    Add files via upload

---
+++ Reflection and Attributes - Lab/06. Code Tracker/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace AuthorProblem
+{
+    [Author("John")]
+    public class StartUp
+    {        
+        public static void Main()
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+
+        [Author("Jane")]
+        public void Test() { }
+    }
+}
