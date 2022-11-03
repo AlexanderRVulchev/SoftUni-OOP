a19158e4f45f941965cc83d8b0862d1f7722512b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-11-03 22:16:02 +0200

    Add files via upload

---
+++ Reflection and Attributes - Lab/02. High Quality Mistakes/Spy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
+        {
+            Type classType = Type.GetType(investigatedClass);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+
+            object classInstance = Activator.CreateInstance(classType);
+            foreach (FieldInfo field in fields)
+            {
+                if (requestedFields.Contains(field.Name))
+                {
+                    sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public string AnalyzeAccessModifiers(string className)
+        {
+            Type classType = Type.GetType(className);
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+            MethodInfo[] publicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] nonPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            StringBuilder sb = new StringBuilder();
+
+            object instance = Activator.CreateInstance(classType);
+
+            foreach (FieldInfo field in fields)
+                sb.AppendLine($"{field.Name} must be private!");
+
+            foreach (MethodInfo method in publicMethods.Where(m => m.Name.StartsWith("set")))
+                sb.AppendLine($"{method.Name} have to be private!");
+
+            foreach (MethodInfo method in nonPublicMethods.Where(m => m.Name.StartsWith("get")))
+                sb.AppendLine($"{method.Name} have to be public!");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/02. High Quality Mistakes/StartUp.cs
@@ -0,0 +1,35 @@
+﻿//NOTE: You need a public StartUp class with the namespace Stealer.
+//You are already an expert on High-Quality Code,
+//so you know what kind of access modifiers must be set to the members of a class.
+//You should have noticed that our hacker is not familiar with these concepts.
+//Create a method inside your Spy class called - AnalyzeAccessModifiers(string className).
+//Check all of the fields and methods access modifiers.Print on the console all of the mistakes in the format:
+//•	Fields
+//o
+//{ fieldName }
+//must be private!
+//•	Getters
+//o
+//{ methodName }
+//have to be public!
+//•	Setters
+//o
+//{ methodName }
+//have to be private!
+//Use StringBuilder to concatenate the answer.Don’t change anything in Hacker class!
+
+
+using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+            string result = spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            Console.WriteLine(result);
+        }
+    }
+}
