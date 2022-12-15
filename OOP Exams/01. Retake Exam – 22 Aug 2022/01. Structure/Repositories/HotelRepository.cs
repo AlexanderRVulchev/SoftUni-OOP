96cdc8f7f0b733e38df2a8d6f8e9ffce27aa8860
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-15 19:07:17 +0200

    Add files via upload

---
+++ OOP Exams/01. Retake Exam – 22 Aug 2022/01. Structure/Repositories/HotelRepository.cs
@@ -0,0 +1,33 @@
+﻿using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Repositories
+{
+    public class HotelRepository : IRepository<IHotel>
+    {
+        private List<IHotel> hotels;
+
+        public HotelRepository()
+        {
+            hotels = new List<IHotel>();
+        }
+
+        public void AddNew(IHotel hotel)
+        {
+            hotels.Add(hotel);
+        }
+
+        public IReadOnlyCollection<IHotel> All()
+        {
+            return hotels.AsReadOnly();
+        }
+
+        public IHotel Select(string hotelName)
+        {
+            return hotels.Find(x => x.FullName == hotelName);
+        }
+    }
+}
