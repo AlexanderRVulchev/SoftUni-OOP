d54ef75fbb018bb3745e1a62cb6dacd242337ca2
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-15 19:07:00 +0200

    Add files via upload

---
+++ OOP Exams/01. Retake Exam – 22 Aug 2022/01. Structure/Models/Rooms/Room.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    using BookingApp.Models.Rooms.Contracts;
+    using BookingApp.Utilities.Messages;
+
+    public abstract class Room : IRoom
+    {
+        private readonly int bedCapacity;
+        private double pricePerNight;
+
+        public Room(int bedCapacity)
+        {
+            bedCapacity = 0;
+            pricePerNight = 0;
+        }
+
+        public int BedCapacity => bedCapacity;
+
+        public double PricePerNight
+        {
+            get => pricePerNight;
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException(ExceptionMessages.PricePerNightNegative);
+                pricePerNight = value;
+            }
+        }
+
+        public void SetPrice(double price)
+        {
+            PricePerNight = price;
+        }
+    }
+}
