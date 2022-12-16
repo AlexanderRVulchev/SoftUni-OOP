500808ba6e5a9bdf189a6ed13b7c2349c5a599bf
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-16 18:24:16 +0200

    Add files via upload

---
+++ OOP Exams/01. Retake Exam – 22 Aug 2022/02. Business Logic/Models/Rooms/Apartment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    public class Apartment : Room
+    {
+        public Apartment() : base(6)
+        { }
+    }
+}
