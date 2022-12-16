1a4740ee1b3b5f4ad79d9f04ea3424b8c9e042fc
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-16 18:24:40 +0200

    Add files via upload

---
+++ OOP Exams/01. Retake Exam – 22 Aug 2022/02. Business Logic/Repositories/BookingRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Repositories
+{
+    using BookingApp.Models.Bookings.Contracts;
+    using BookingApp.Repositories.Contracts;
+
+    public class BookingRepository : IRepository<IBooking>
+    {
+        private List<IBooking> bookings;
+
+        public BookingRepository()
+        {
+            bookings = new List<IBooking>();
+        }
+
+        public void AddNew(IBooking booking)
+        {
+            bookings.Add(booking);
+        }
+
+        public IReadOnlyCollection<IBooking> All()
+        {
+            return bookings.AsReadOnly();
+        }
+
+        public IBooking Select(string bookingNumberToString)
+        {
+            return bookings.Find(x => x.BookingNumber.ToString() == bookingNumberToString);
+        }
+    }
+}
