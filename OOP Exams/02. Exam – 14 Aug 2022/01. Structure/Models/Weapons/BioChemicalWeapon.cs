c49f53c3d7ea3118c6ad36433169daada0882dcf
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-18 15:06:54 +0200

    Add files via upload

---
+++ OOP Exams/02. Exam – 14 Aug 2022/01. Structure/Models/Weapons/BioChemicalWeapon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public class BioChemicalWeapon : Weapon
+    {
+        public BioChemicalWeapon(int destructionLevel)
+            : base(destructionLevel, 3.2)
+        { }
+    }
+}
---
+++ OOP Exams/02. Exam – 14 Aug 2022/01. Structure/Models/Weapons/Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    using Models.Weapons.Contracts;
+    using Utilities.Messages;
+
+    public abstract class Weapon : IWeapon
+    {
+        private int destructionLevel;
+        private double price;
+
+        public Weapon(int destructionLevel, double price)
+        {
+            Price = price;
+            DestructionLevel = destructionLevel;
+        }
+
+        public double Price
+        {
+            get => price;
+            private set => price = value;
+        }
+
+        public int DestructionLevel
+        {
+            get => destructionLevel;
+            private set
+            {
+                if (value < 1)                
+                    throw new ArgumentException(ExceptionMessages.TooLowDestructionLevel);                
+                if (value > 10)                
+                    throw new ArgumentException(ExceptionMessages.TooHighDestructionLevel);
+                destructionLevel = value;
+            }
+        }
+    }
+}
