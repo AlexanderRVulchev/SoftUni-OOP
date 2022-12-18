2798dd0af4e72837a227fb8481baf92d4a0e15f9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-18 15:06:22 +0200

    Add files via upload

---
+++ OOP Exams/02. Exam – 14 Aug 2022/01. Structure/Models/MilitaryUnits/MilitaryUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    using Models.MilitaryUnits.Contracts;
+    using Utilities.Messages;
+
+    public abstract class MilitaryUnit : IMilitaryUnit
+    {
+        private double cost;
+        private int enduraceLevel;
+
+        public MilitaryUnit(double cost)
+        {
+            this.cost = cost;
+            enduraceLevel = 1;
+        }
+
+        public double Cost => cost;
+
+        public int EnduranceLevel => enduraceLevel;
+
+        public void IncreaseEndurance()
+        {
+            if (enduraceLevel == 20)
+                throw new ArgumentException(ExceptionMessages.EnduranceLevelExceeded);
+            else
+                enduraceLevel++;
+        }
+    }
+}
---
+++ OOP Exams/02. Exam – 14 Aug 2022/01. Structure/Models/MilitaryUnits/SpaceForces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.MilitaryUnits
+{
+    public class SpaceForces : MilitaryUnit
+    {
+        public SpaceForces() 
+            : base(11) 
+        { }
+    }
+}
