a705bd067058f2226e954a7cb87f0a561801cc10
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-19 20:01:37 +0200

    Add files via upload

---
+++ OOP Exams/02. Exam – 14 Aug 2022/02. Business Logic/Models/Rooms/DoubleBed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    public class DoubleBed : Room
+    {
+        public DoubleBed() : base(2)
+        { }
+    }
+}
---
+++ OOP Exams/02. Exam – 14 Aug 2022/02. Business Logic/Models/Rooms/Studio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Models.Rooms
+{
+    public class Studio : Room
+    {
+        public Studio() : base(4)
+        { }
+    }
+}
