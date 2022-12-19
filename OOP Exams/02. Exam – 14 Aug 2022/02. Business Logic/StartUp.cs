9048add3185304eb512ae743f3b58cef9f976c31
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-19 19:59:04 +0200

    Add files via upload

---
+++ OOP Exams/02. Exam – 14 Aug 2022/02. Business Logic/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace BookingApp
+{
+    using BookingApp.Core;
+    using BookingApp.Core.Contracts;
+    using BookingApp.Models.Bookings;
+    using BookingApp.Models.Hotels;
+    using BookingApp.Models.Rooms;
+    using BookingApp.Models.Rooms.Contracts;
+    using BookingApp.Repositories;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // Don't forget to comment out the commented code lines in the Engine class!
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
