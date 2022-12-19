f89064e34e511c1f30b67b1a03317f66312df3ce
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-19 20:01:55 +0200

    Add files via upload

---
+++ OOP Exams/02. Exam – 14 Aug 2022/02. Business Logic/Repositories/RoomRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookingApp.Repositories
+{    
+    using BookingApp.Models.Rooms.Contracts;
+    using BookingApp.Repositories.Contracts;
+
+    public class RoomRepository : IRepository<IRoom>
+    {
+        private List<IRoom> rooms;
+
+        public RoomRepository()
+        {
+            rooms = new List<IRoom>();
+        }
+
+        public void AddNew(IRoom room)
+        {
+            rooms.Add(room);
+        }
+
+        public IRoom Select(string roomtypeName)
+        {
+            return rooms.Find(x => x.GetType().Name == roomtypeName);
+        }
+
+        public IReadOnlyCollection<IRoom> All()
+        {
+            return rooms.AsReadOnly();
+        }
+    }
+}
