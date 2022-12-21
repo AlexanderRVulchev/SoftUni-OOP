05b9f41e7fe56ac9c408bd708df11c74b58e3541
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-21 21:36:24 +0200

    Add files via upload

---
+++ OOP Exams/03. Retake Exam - 18 April 2022/01. Structure/Models/Weapons/Claymore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Claymore : Weapon
+    {
+        private const int DAMAGE = 20;
+
+        public Claymore(string name, int durability)
+            : base(name, durability)
+        { }
+
+        public override int DoDamage()
+        {
+            this.Durability--;
+            if (this.Durability == 0) return 0;
+            else return DAMAGE;
+        }
+    }
+}
