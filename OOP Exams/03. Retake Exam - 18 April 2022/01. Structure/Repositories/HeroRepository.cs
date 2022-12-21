07ff97c92450544097473ac01c70cf6fb476fb1c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-21 21:36:54 +0200

    Add files via upload

---
+++ OOP Exams/03. Retake Exam - 18 April 2022/01. Structure/Repositories/HeroRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    using Models.Contracts;
+    using Models.Heroes;
+    using Repositories.Contracts;
+
+    public class HeroRepository : IRepository<IHero>
+    {
+        private List<IHero> heroes;
+
+        public HeroRepository()
+        {
+            heroes = new List<IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Models => heroes.AsReadOnly();
+
+        public void Add(IHero model)
+        {
+            heroes.Add(model);
+        }
+
+        public IHero FindByName(string name)
+        {
+            return heroes.Find(h => h.Name == name);
+        }
+
+        public bool Remove(IHero model)
+        {
+            return heroes.Remove(model);
+        }
+    }
+}
