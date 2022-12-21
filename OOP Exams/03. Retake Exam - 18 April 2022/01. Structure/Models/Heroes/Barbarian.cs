b33487b536d9e503d79722cb03bf15664442890f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-21 21:35:49 +0200

    Add files via upload

---
+++ OOP Exams/03. Retake Exam - 18 April 2022/01. Structure/Models/Heroes/Barbarian.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Heroes
+{
+    public class Barbarian : Hero
+    {
+        public Barbarian(string name, int health, int armour)
+            : base(name, health, armour)
+        { }
+    }
+}
---
+++ OOP Exams/03. Retake Exam - 18 April 2022/01. Structure/Models/Heroes/Knight.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Heroes
+{
+    public class Knight : Hero
+    {
+        public Knight(string name, int health, int armour) 
+            : base(name, health, armour)
+        { }
+    }
+}
