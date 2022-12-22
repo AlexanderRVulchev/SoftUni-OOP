c7122762fea497c5e939bd00f2267c172381e5b8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-22 20:19:36 +0200

    Add files via upload

---
+++ OOP Exams/03. Retake Exam - 18 April 2022/02. Business Logic/Models/Weapons/Mace.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Mace : Weapon
+    {
+        private const int DAMAGE = 25;
+
+        public Mace(string name, int durability)
+            : base(name, durability)
+        { }
+
+        public override int DoDamage()
+        {
+            if (Durability > 0)
+                Durability--;
+            if (Durability == 0)
+                return 0;
+            else
+                return DAMAGE;
+        }
+    }
+}
---
+++ OOP Exams/03. Retake Exam - 18 April 2022/02. Business Logic/Models/Weapons/Weapon.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    using Models.Contracts;
+
+    public abstract class Weapon : IWeapon
+    {
+        private string name;
+        private int durability;
+
+        public Weapon(string name, int durability)
+        {
+            this.Name = name;
+            this.Durability = durability;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Weapon type cannot be null or empty.");
+                name = value;
+            }
+        }
+
+        public int Durability
+        {
+            get { return durability; }
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Durability cannot be below 0.");
+                durability = value;
+            }
+        }
+
+        public abstract int DoDamage();
+    }
+}
