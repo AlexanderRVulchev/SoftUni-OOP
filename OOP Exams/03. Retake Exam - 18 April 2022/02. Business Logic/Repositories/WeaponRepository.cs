b4331273a222b2599c75c614397bdfdc6beb3a61
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-22 20:20:27 +0200

    Add files via upload

---
+++ OOP Exams/03. Retake Exam - 18 April 2022/02. Business Logic/Repositories/WeaponRepository.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Repositories
+{
+    using Models.Contracts;
+    using Models.Weapons;
+    using Repositories.Contracts;
+
+    public class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> weapons;
+
+        public WeaponRepository()
+        {
+            weapons = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => weapons.AsReadOnly();
+
+        public void Add(IWeapon model)
+        {
+            weapons.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            return weapons.Find(w => w.Name == name);
+        }
+
+        public bool Remove(IWeapon model)
+        {
+            return weapons.Remove(model);
+        }
+    }
+}
