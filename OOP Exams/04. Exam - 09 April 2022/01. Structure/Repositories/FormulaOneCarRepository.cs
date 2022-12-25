6bacd5121c837ee124a575cfd45c5679d3f86965
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-25 11:23:50 +0200

    Add files via upload

---
+++ OOP Exams/04. Exam - 09 April 2022/01. Structure/Repositories/FormulaOneCarRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    using Repositories.Contracts;
+    using Models.Contracts;
+
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
+    {
+        private List<IFormulaOneCar> cars;
+
+        public FormulaOneCarRepository()
+        {
+            cars = new List<IFormulaOneCar>();
+        }
+
+        public IReadOnlyCollection<IFormulaOneCar> Models => cars.AsReadOnly();
+
+        public void Add(IFormulaOneCar model)
+        {
+            cars.Add(model);
+        }
+
+        public IFormulaOneCar FindByName(string name)
+        {
+            return cars.Find(x => x.Model == name);
+        }
+
+        public bool Remove(IFormulaOneCar model)
+        {
+            return cars.Remove(model);
+        }
+    }
+}
