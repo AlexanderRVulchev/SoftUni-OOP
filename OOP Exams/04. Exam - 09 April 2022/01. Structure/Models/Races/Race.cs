5f19a227bbd9c746cce6ed7fb4c689eb51f49414
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-25 11:23:32 +0200

    Add files via upload

---
+++ OOP Exams/04. Exam - 09 April 2022/01. Structure/Models/Races/Race.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models.Races
+{
+    using Models.Contracts;
+
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+        private List<IPilot> pilots;
+
+        public Race(string raceName, int numberOfLaps)
+        {
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+            TookPlace = false;
+            pilots = new List<IPilot>();
+        }
+
+        public string RaceName
+        {
+            get { return raceName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                    throw new ArgumentException(string.Format(Utilities.ExceptionMessages.InvalidRaceName, value));
+                raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get { return numberOfLaps; }
+            private set
+            {
+                if (value < 1)
+                    throw new ArgumentException(String.Format(Utilities.ExceptionMessages.InvalidLapNumbers, value));
+                numberOfLaps = value;
+            }
+        }
+
+        public bool TookPlace { get; set; }
+
+        public ICollection<IPilot> Pilots => pilots.AsReadOnly();
+
+        public void AddPilot(IPilot pilot)
+        {
+            pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            string tookPlace = TookPlace ? "Yes" : "No";
+
+            sb.AppendLine($"The {RaceName} race has:");
+            sb.AppendLine($"Participants: {pilots.Count}");
+            sb.AppendLine($"Number of laps: {NumberOfLaps}");            
+            sb.Append($"Took place: {tookPlace}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
