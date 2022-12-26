8451b2b8af894bc9b66a6afa2caf123afa1eef22
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2022-12-26 11:56:24 +0200

    Add files via upload

---
+++ OOP Exams/04. Exam - 09 April 2022/02. Business Logic/Repositories/PilotRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    using Models.Contracts;
+    using Repositories.Contracts;
+
+    public class PilotRepository : IRepository<IPilot>
+    {
+        private List<IPilot> pilots;
+
+        public PilotRepository()
+        {
+            pilots = new List<IPilot>();
+        }
+
+        public IReadOnlyCollection<IPilot> Models => pilots.AsReadOnly();
+
+        public void Add(IPilot model)
+        {
+            pilots.Add(model);
+        }
+
+        public IPilot FindByName(string name)
+        {
+            return pilots.Find(x => x.FullName == name);
+        }
+
+        public bool Remove(IPilot model)
+        {
+            return pilots.Remove(model);
+        }
+    }
+}
---
+++ OOP Exams/04. Exam - 09 April 2022/02. Business Logic/Repositories/RaceRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    using Formula1.Models.Contracts;
+    using Repositories.Contracts;
+
+    public class RaceRepository : IRepository<IRace>
+    {
+        private List<IRace> races;
+
+        public RaceRepository()
+        {
+            races = new List<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => races.AsReadOnly();
+
+        public void Add(IRace model)
+        {
+            races.Add(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            return races.Find(x => x.RaceName == name);
+        }
+
+        public bool Remove(IRace model)
+        {
+            return races.Remove(model);
+        }
+    }
+}
