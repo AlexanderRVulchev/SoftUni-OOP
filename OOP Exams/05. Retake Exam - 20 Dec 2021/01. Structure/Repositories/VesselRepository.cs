38b2999ea21503b4bc69dfdd4dbbcae381f6e9c3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-01-31 18:21:17 +0200

    Add files via upload

---
+++ OOP Exams/05. Retake Exam - 20 Dec 2021/01. Structure/Repositories/VesselRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Repositories
+{
+    using Models.Contracts;
+    using Repositories.Contracts;
+    
+    public class VesselRepository : IRepository<IVessel>
+    {
+        private List<IVessel> vessels;
+
+        public IReadOnlyCollection<IVessel> Models => vessels.AsReadOnly();
+
+        public void Add(IVessel model)
+        {
+            vessels.Add(model);
+        }
+
+        public IVessel FindByName(string name)
+        {
+            return vessels.Find(x => x.Name == name);
+        }
+
+        public bool Remove(IVessel model)
+        {
+            return vessels.Remove(model);
+        }
+    }
+}
