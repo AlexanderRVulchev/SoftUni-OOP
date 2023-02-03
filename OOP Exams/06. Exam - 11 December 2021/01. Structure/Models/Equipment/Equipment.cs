6c78a8d8ae94b4ccb8f1caaca8128e84e620700a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-03 18:41:16 +0200

    Add files via upload

---
+++ OOP Exams/06. Exam - 11 December 2021/01. Structure/Models/Equipment/Equipment.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Equipment
+{
+    using Models.Equipment.Contracts;
+
+    public abstract class Equipment : IEquipment
+    {
+        private double weight;
+        private decimal price;
+
+        public Equipment(double weight, decimal price)
+        {
+            Weight = weight;
+            Price = price;
+        }
+
+        public double Weight
+        {
+            get => weight;
+            private set => weight = value;
+        }
+
+        public decimal Price
+        {
+            get => price;
+            private set => price = value;
+        }
+    }
+}
