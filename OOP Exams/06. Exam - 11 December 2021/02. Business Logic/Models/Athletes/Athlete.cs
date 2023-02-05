664e938563510c991441d0e21b7992009009c82f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-05 17:22:41 +0200

    Add files via upload

---
+++ OOP Exams/06. Exam - 11 December 2021/02. Business Logic/Models/Athletes/Athlete.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Models.Athletes
+{
+    using Models.Athletes.Contracts;
+    using Utilities.Messages;
+
+    public abstract class Athlete : IAthlete
+    {
+        private string fullName;
+        private string motivation;
+        private int stamina;
+        private int numberOfMedals;
+
+        public Athlete(string fullName, string motivation, int numberOfMedals, int stamina)
+        {
+            FullName = fullName;
+            Motivation = motivation;
+            NumberOfMedals = numberOfMedals;
+            Stamina = stamina;
+        }
+
+        public string FullName
+        {
+            get => fullName;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteName);
+                fullName = value;
+            }
+        }
+
+        public string Motivation
+        {
+            get => motivation;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMotivation);
+                motivation = value;
+            }
+        }
+        
+        public int Stamina
+        {
+            get => stamina;
+            protected set
+            {
+                if (value > 100)
+                {
+                    stamina = 100;
+                    throw new ArgumentException(ExceptionMessages.InvalidStamina);
+                }
+                else
+                {
+                    stamina = value;
+                }
+            }
+        }
+ 
+        public int NumberOfMedals
+        {
+            get => numberOfMedals;
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException(ExceptionMessages.InvalidAthleteMedals);
+                numberOfMedals = value;
+            }
+        }
+
+        public abstract void Exercise();        
+    }
+}
