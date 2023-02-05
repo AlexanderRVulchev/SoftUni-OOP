70c57cf6ef75ca9245509d80ae0c3e5dc9ebfefe
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-05 17:23:35 +0200

    Add files via upload

---
+++ OOP Exams/06. Exam - 11 December 2021/02. Business Logic/Repositories/EquipmentRepository.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Repositories
+{
+    using Models.Equipment.Contracts;
+    using Repositories.Contracts;
+
+    public class EquipmentRepository : IRepository<IEquipment>
+    {
+        private List<IEquipment> equipment;
+
+        public EquipmentRepository()
+        {
+            equipment = new List<IEquipment>();
+        }
+
+        public IReadOnlyCollection<IEquipment> Models => equipment.AsReadOnly();
+
+        public void Add(IEquipment model)
+        {
+            equipment.Add(model);
+        }
+
+        public IEquipment FindByType(string type)
+        => equipment.Find(x => x.GetType().Name == type);
+        
+
+        public bool Remove(IEquipment model)
+        => equipment.Remove(model);
+    }
+}
