1f16729221c5954d2ee470cb557bfd4ee6b62881
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-11 14:47:45 +0200

    Add files via upload

---
+++ OOP Exams/07. Retake Exam - 22 August 2021/01. Structure/Repositories/PlanetRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Repositories
+{
+    using Contracts;
+    using Models.Planets.Contracts;
+
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private List<IPlanet> models;
+
+        public PlanetRepository()
+        {
+            models = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => models.AsReadOnly();
+
+        public void Add(IPlanet model)
+        {
+            models.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+        => models.Find(x => x.Name == name);
+
+        public bool Remove(IPlanet model)
+        => models.Remove(model);
+    }
+}
