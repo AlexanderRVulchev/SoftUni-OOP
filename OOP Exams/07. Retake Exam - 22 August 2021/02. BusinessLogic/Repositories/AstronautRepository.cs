71d759ab7e0f017f3643c272d81f61bf0f19c892
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-12 14:14:33 +0200

    Add files via upload

---
+++ OOP Exams/07. Retake Exam - 22 August 2021/02. BusinessLogic/Repositories/AstronautRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Repositories
+{
+    using Contracts;
+    using Models.Astronauts.Contracts;
+    
+    public class AstronautRepository : IRepository<IAstronaut>
+    {
+        private List<IAstronaut> models;
+
+        public AstronautRepository()
+        {
+            models = new List<IAstronaut>();
+        }
+
+        public IReadOnlyCollection<IAstronaut> Models => models.AsReadOnly();
+
+        public void Add(IAstronaut model)
+        {
+            models.Add(model);
+        }
+
+        public IAstronaut FindByName(string name)
+        => models.Find(x => x.Name == name);
+
+        public bool Remove(IAstronaut model)
+        => models.Remove(model);
+    }
+}
