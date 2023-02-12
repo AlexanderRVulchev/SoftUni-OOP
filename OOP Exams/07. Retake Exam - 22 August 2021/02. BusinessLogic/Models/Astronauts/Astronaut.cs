1d48302d9d18709fa2aa228af8e11560375921cb
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-12 14:15:20 +0200

    Add files via upload

---
+++ OOP Exams/07. Retake Exam - 22 August 2021/02. BusinessLogic/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    using Contracts;
+    using Bags.Contracts;
+    using Utilities.Messages;
+    using SpaceStation.Models.Bags;
+
+    public abstract class Astronaut : IAstronaut
+    {
+        private string name;
+        private double oxygen;
+        private IBag bag;
+
+        public Astronaut(string name, double oxygen)
+        {
+            Name = name;
+            Oxygen = oxygen;
+            bag = new Backpack();
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(ExceptionMessages.InvalidAstronautName);
+                name = value;
+            }
+        }
+
+        public double Oxygen
+        {
+            get => oxygen;
+            protected set
+            {
+                if (value < 0)
+                    throw new ArgumentException(ExceptionMessages.InvalidOxygen);
+                oxygen = value;
+            }
+        }
+
+        public bool CanBreath => oxygen > 0;
+
+        public IBag Bag => bag;
+
+        public virtual void Breath()
+        {            
+            Oxygen = Math.Max(Oxygen - 10, 0);
+        }
+    }
+}
---
+++ OOP Exams/07. Retake Exam - 22 August 2021/02. BusinessLogic/Models/Astronauts/Meteorologist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public class Meteorologist : Astronaut
+    {
+        public Meteorologist(string name)
+            : base(name, 90)
+        { }
+    }
+}
