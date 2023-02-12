1cd446c254d99702550cfd164b5747f349ceb01e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-12 14:16:09 +0200

    Add files via upload

---
+++ OOP Exams/07. Retake Exam - 22 August 2021/02. BusinessLogic/Models/Planets/Planet.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Planets
+{
+    using Contracts;
+    using Utilities.Messages;
+
+    public class Planet : IPlanet
+    {
+        private string name;
+        private List<string> items;
+
+        public Planet(string name)
+        {
+            Name = name;
+            items = new List<string>();
+        }
+
+        public ICollection<string> Items => items;
+                
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(ExceptionMessages.InvalidPlanetName);
+                name = value;
+            }
+        }
+    }
+}
