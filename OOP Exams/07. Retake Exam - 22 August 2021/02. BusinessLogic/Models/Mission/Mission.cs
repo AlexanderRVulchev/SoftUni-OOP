113ba582fe4c0980c3893f187e08de4d038c1ac3
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-12 14:15:53 +0200

    Add files via upload

---
+++ OOP Exams/07. Retake Exam - 22 August 2021/02. BusinessLogic/Models/Mission/Mission.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+{
+    using Contracts;
+    using Models.Astronauts.Contracts;
+    using Models.Planets.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Mission : IMission
+    {
+
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            foreach (IAstronaut astronaut in astronauts)
+            {
+                while (astronaut.CanBreath && planet.Items.Any())
+                {
+                    string item = planet.Items.First();
+                    astronaut.Bag.Items.Add(item);
+                    planet.Items.Remove(item);
+                    astronaut.Breath();
+                }
+            }
+        }
+    }
+}
