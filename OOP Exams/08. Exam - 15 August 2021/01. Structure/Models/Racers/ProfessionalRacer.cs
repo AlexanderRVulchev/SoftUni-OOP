ade4e6130c3f303db2c37e94584cf0944314314e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-18 16:08:29 +0200

    Add files via upload

---
+++ OOP Exams/08. Exam - 15 August 2021/01. Structure/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    using Cars.Contracts;
+
+    public class ProfessionalRacer : Racer
+    {
+        public ProfessionalRacer(string username, ICar car)
+            : base(username, "strict", 30, car)
+        { }
+
+        public override void Race()
+        {
+            base.Race();
+            DrivingExperience += 10;
+        }
+    }
+}
