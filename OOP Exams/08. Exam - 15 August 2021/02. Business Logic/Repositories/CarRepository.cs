1caf857306fdf85b464b19463de87a5e621fdbcc
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-19 11:07:41 +0200

    Add files via upload

---
+++ OOP Exams/08. Exam - 15 August 2021/02. Business Logic/Repositories/CarRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Repositories
+{
+    using Models.Cars.Contracts;
+    using Contracts;
+    using Utilities.Messages;
+    
+    public class CarRepository : IRepository<ICar>
+    {
+        private List<ICar> models;
+
+        public CarRepository()
+        {
+            models = new List<ICar>();
+        }
+
+        public IReadOnlyCollection<ICar> Models => models.AsReadOnly();
+
+        public void Add(ICar model)
+        {
+            if (model == null)
+                throw new ArgumentException(ExceptionMessages.InvalidAddCarRepository);
+            models.Add(model);
+        }
+        
+        public ICar FindBy(string property)
+        => models.Find(x => x.VIN == property);
+
+        public bool Remove(ICar model)
+        => models.Remove(model);
+    }
+}
---
+++ OOP Exams/08. Exam - 15 August 2021/02. Business Logic/Repositories/RacerRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Repositories
+{
+    using Models.Racers.Contracts;
+    using Contracts;
+    using Utilities.Messages;
+
+    public class RacerRepository : IRepository<IRacer>
+    {
+        private List<IRacer> models;
+
+        public RacerRepository()
+        {
+            models = new List<IRacer>();
+        }
+
+        public IReadOnlyCollection<IRacer> Models => models.AsReadOnly();
+
+        public void Add(IRacer model)
+        {
+            if (model == null)
+                throw new ArgumentException(ExceptionMessages.InvalidAddRacerRepository);
+            models.Add(model);
+        }
+
+        public IRacer FindBy(string property)
+        => models.Find(x => x.Username == property);
+
+        public bool Remove(IRacer model)
+        => models.Remove(model);
+    }
+}
