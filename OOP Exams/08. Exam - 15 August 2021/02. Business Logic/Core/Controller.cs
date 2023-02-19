dc220c02d3c978887c7c2e0ef62027c00e521116
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-19 11:06:50 +0200

    Add files via upload

---
+++ OOP Exams/08. Exam - 15 August 2021/02. Business Logic/Core/Controller.cs
@@ -0,0 +1,76 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Core
+{
+    using Repositories;
+    using Contracts;
+    using Models.Maps.Contracts;
+    using Models.Maps;
+    using CarRacing.Models.Cars;
+    using Utilities.Messages;
+    using CarRacing.Models.Cars.Contracts;
+    using CarRacing.Models.Racers;
+    using CarRacing.Models.Racers.Contracts;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class Controller : IController
+    {
+        private CarRepository cars;
+        private RacerRepository racers;
+        private IMap map;
+
+        public Controller()
+        {
+            cars = new CarRepository();
+            racers = new RacerRepository();
+            map = new Map();
+        }
+
+        public string AddCar(string type, string make, string model, string VIN, int horsePower)
+        {
+            ICar carToAdd;
+            switch (type)
+            {
+                case "SuperCar": carToAdd = new SuperCar(make, model, VIN, horsePower); break;
+                case "TunedCar": carToAdd = new TunedCar(make, model, VIN, horsePower); break;
+                default: throw new ArgumentException(ExceptionMessages.InvalidCarType);
+            }
+            cars.Add(carToAdd);
+            return String.Format(OutputMessages.SuccessfullyAddedCar, make, model, VIN);
+        }
+
+        public string AddRacer(string type, string username, string carVIN)
+        {
+            ICar car = cars.FindBy(carVIN);
+            if (car == null)
+                throw new ArgumentException(ExceptionMessages.CarCannotBeFound);
+            IRacer racer;
+            switch (type)
+            {
+                case "ProfessionalRacer": racer = new ProfessionalRacer(username, car); break;
+                case "StreetRacer": racer = new StreetRacer(username, car); break;
+                default: throw new ArgumentException(ExceptionMessages.InvalidRacerType);
+            }
+            racers.Add(racer);
+            return String.Format(OutputMessages.SuccessfullyAddedRacer, username);
+        }
+
+        public string BeginRace(string racerOneUsername, string racerTwoUsername)
+        {
+            IRacer first = racers.FindBy(racerOneUsername);
+            if (first == null)
+                throw new ArgumentException(String.Format(ExceptionMessages.RacerCannotBeFound, racerOneUsername));
+            IRacer second = racers.FindBy(racerTwoUsername);
+            if (second == null)
+                throw new ArgumentException(String.Format(ExceptionMessages.RacerCannotBeFound, racerTwoUsername));
+            return map.StartRace(first, second);
+        }
+
+        public string Report()
+        => String.Join(Environment.NewLine, racers.Models.OrderByDescending(x => x.DrivingExperience).ThenBy(x => x.Username));
+    }
+}
