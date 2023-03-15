b08fb0c9f4c99a1d47a62886fdea68ea0c5e96ae
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-15 21:34:46 +0200

    Add files via upload

---
+++ OOP Exams/09. Retake Exam - 18 April 2021/01. Structure/Repositories/EggRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Repositories
+{
+    using Contracts;
+    using Models.Eggs.Contracts;
+
+    public class EggRepository : IRepository<IEgg>
+    {
+        private List<IEgg> models;
+
+        public EggRepository()
+        {
+            models = new List<IEgg>();
+        }
+
+        public IReadOnlyCollection<IEgg> Models => models.AsReadOnly();
+
+        public void Add(IEgg model)
+        {
+            models.Add(model);
+        }
+
+        public IEgg FindByName(string name)
+        => models.Find(x => x.Name == name);
+
+        public bool Remove(IEgg model)
+        => models.Remove(model);
+    }
+}
