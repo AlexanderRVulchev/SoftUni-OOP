0dc2a3f03be3345013fe972839fdf5967b6709b9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-16 18:47:15 +0200

    Add files via upload

---
+++ OOP Exams/09. Retake Exam - 18 April 2021/02. Business Logic/Models/Bunnies/SleepyBunny.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Bunnies
+{
+    internal class SleepyBunny : Bunny
+    {
+        public SleepyBunny(string name)
+            : base(name, 50)
+        { }
+
+        public override void Work()
+        {
+            Energy -= 15;
+        }
+    }
+}
---
+++ OOP Exams/09. Retake Exam - 18 April 2021/02. Business Logic/Models/Eggs/Egg.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Eggs
+{
+    using Contracts;
+    using Utilities.Messages;
+
+    public class Egg : IEgg
+    {
+        private string name;
+        private int energyRequired;
+
+        public Egg(string name, int energyRequired)
+        {
+            Name = name;
+            EnergyRequired = energyRequired;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException(ExceptionMessages.InvalidEggName);
+                name = value;
+            }
+        }
+
+        public int EnergyRequired
+        {
+            get { return energyRequired; }
+            private set
+            {
+                if (value < 0)
+                    energyRequired = 0;
+                else
+                    energyRequired = value;
+            }
+        }
+
+        public void GetColored()
+        {
+            EnergyRequired -= 10;
+        }
+
+        public bool IsDone()
+        => energyRequired == 0;
+    }
+}
