a3d207ae323dbfe6935b97d2d3ac7ce5497689d8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-16 18:47:36 +0200

    Add files via upload

---
+++ OOP Exams/09. Retake Exam - 18 April 2021/02. Business Logic/Repositories/BunnyRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Repositories
+{
+    using Contracts;
+    using Models.Bunnies.Contracts;
+
+    public class BunnyRepository : IRepository<IBunny>
+    {
+        private List<IBunny> models;
+
+        public BunnyRepository()
+        {
+            models = new List<IBunny>();
+        }
+
+        public IReadOnlyCollection<IBunny> Models => models.AsReadOnly();
+
+        public void Add(IBunny model)
+        {
+            models.Add(model);
+        }
+
+        public IBunny FindByName(string name)
+        => models.Find(x => x.Name == name);
+
+        public bool Remove(IBunny model)
+        => models.Remove(model);
+    }
+}
