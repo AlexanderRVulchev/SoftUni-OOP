6ca4d257bdc90fa120fcf2c2acab66f1efc39844
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-18 17:01:20 +0200

    Add files via upload

---
+++ OOP Exams/10. Exam - 10 April 2021/01. Structure/Models/Decorations/Ornament.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Models.Decorations
+{
+    public class Ornament : Decoration
+    {
+        public Ornament()
+            : base(1, 5)
+        { }
+    }
+}
