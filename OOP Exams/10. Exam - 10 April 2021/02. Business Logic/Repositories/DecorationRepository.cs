023da5363e727892db27c84e6061cc89ba6bbccd
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-20 17:55:22 +0200

    Add files via upload

---
+++ OOP Exams/10. Exam - 10 April 2021/02. Business Logic/Repositories/DecorationRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AquaShop.Repositories
+{
+    using Models.Decorations.Contracts;
+    using Contracts;
+
+    public class DecorationRepository : IRepository<IDecoration>
+    {
+        private List<IDecoration> models;
+
+        public DecorationRepository()
+        {
+            models = new List<IDecoration>();
+        }
+
+        public IReadOnlyCollection<IDecoration> Models => models.AsReadOnly();
+
+        public void Add(IDecoration model)
+        {
+            models.Add(model);
+        }
+
+        public IDecoration FindByType(string type)
+        => models.Find(x => x.GetType().Name == type);
+
+        public bool Remove(IDecoration model)
+        => models.Remove(model);
+    }
+}
