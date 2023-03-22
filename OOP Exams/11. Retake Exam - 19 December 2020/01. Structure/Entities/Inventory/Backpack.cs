6eafaf0f9497db6902cafa7f69f04a99c3c07cdd
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-22 19:03:44 +0200

    Add files via upload

---
+++ OOP Exams/11. Retake Exam - 19 December 2020/01. Structure/Entities/Inventory/Backpack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarCroft.Entities.Inventory
+{
+    public class Backpack : Bag
+    {
+        public Backpack()
+            : base(100)
+        { }
+    }
+}
---
+++ OOP Exams/11. Retake Exam - 19 December 2020/01. Structure/Entities/Inventory/Bag.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WarCroft.Entities.Inventory
+{
+    using Constants;
+    using Entities.Items;
+
+    public abstract class Bag : IBag
+    {
+        private List<Item> items;
+
+        public Bag(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public int Capacity { get; set; } = 100;
+
+        public int Load => items.Select(x => x.Weight).Sum();
+
+        public IReadOnlyCollection<Item> Items => items.AsReadOnly();
+
+        public void AddItem(Item item)
+        {
+            if (Load + item.Weight > Capacity)
+                throw new InvalidOperationException(ExceptionMessages.ExceedMaximumBagCapacity);
+            items.Add(item);
+        }
+
+        public Item GetItem(string name)
+        {            
+            if (!items.Any())
+                throw new InvalidOperationException(ExceptionMessages.EmptyBag);
+            Item item = items.Find(x => x.GetType().Name == name);
+            if (item == null)
+                throw new ArgumentException(String.Format(ExceptionMessages.ItemNotFoundInBag, name));
+            items.Remove(item);
+            return item;
+        }
+    }
+}
