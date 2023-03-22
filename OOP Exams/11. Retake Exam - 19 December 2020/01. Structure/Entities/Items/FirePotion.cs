2052ebd0f45ce246d7092712c45da8a61db6c30f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-22 19:04:04 +0200

    Add files via upload

---
+++ OOP Exams/11. Retake Exam - 19 December 2020/01. Structure/Entities/Items/FirePotion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WarCroft.Entities.Characters.Contracts;
+
+namespace WarCroft.Entities.Items
+{
+    internal class FirePotion : Item
+    {
+        public FirePotion()
+            : base(5)
+        { }
+
+        public override void AffectCharacter(Character character)
+        {
+            base.AffectCharacter(character);
+            character.TakeDamage(20);            
+        }
+    }
+}
