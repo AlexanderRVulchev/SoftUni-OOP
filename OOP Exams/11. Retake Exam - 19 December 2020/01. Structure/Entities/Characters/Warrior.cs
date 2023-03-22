15e791c85bd39640aaf4847ee7ef6fb32df9930e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-22 19:03:23 +0200

    Add files via upload

---
+++ OOP Exams/11. Retake Exam - 19 December 2020/01. Structure/Entities/Characters/Warrior.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarCroft.Entities.Characters
+{
+    using Contracts;
+    using WarCroft.Constants;
+    using WarCroft.Entities.Inventory;
+
+    internal class Warrior : Character, IAttacker
+    {
+        public Warrior(string name)
+            : base(name, 100, 50, 40, new Satchel())
+        { }
+
+        public void Attack(Character character)
+        {
+            this.EnsureAlive();
+            if (!character.IsAlive)
+                return;
+            if (character.Equals(this))
+                throw new InvalidOperationException(ExceptionMessages.CharacterAttacksSelf);
+            character.TakeDamage(this.AbilityPoints);
+        }
+    }
+}
