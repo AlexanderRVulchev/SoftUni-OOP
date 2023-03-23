15444911b1cc443474b52b686816116500136659
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-23 18:32:04 +0200

    Add files via upload

---
+++ OOP Exams/11. Retake Exam - 19 December 2020/02. Business Logic/Entities/Inventory/Satchel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarCroft.Entities.Inventory
+{
+    public class Satchel : Bag
+    {
+        public Satchel()
+            : base(20)
+        { }
+    }
+}
