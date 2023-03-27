5922d595f4991cd40d1aeaf027fb568529d781c9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-27 18:33:02 +0300

    Add files via upload

---
+++ OOP Exams/12. Exam - 12 December 2020/02. Business Logic/Models/Drinks/Tea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bakery.Models.Drinks
+{
+    public class Tea : Drink
+    {
+        private const decimal TeaPrice = 2.50M;
+
+        public Tea(string name, int portion, string brand)
+            : base(name, portion, TeaPrice, brand)
+        { }
+    }
+}
