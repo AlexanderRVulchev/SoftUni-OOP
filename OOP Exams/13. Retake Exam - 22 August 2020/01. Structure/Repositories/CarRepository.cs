058b40cea60adab3b15dd529f5a81c1e5a90143d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-31 18:26:02 +0300

    Add files via upload

---
+++ OOP Exams/13. Retake Exam - 22 August 2020/01. Structure/Repositories/CarRepository.cs
@@ -0,0 +1,15 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories
+{
+    public class CarRepository : Repository<ICar>
+    {
+        public override ICar GetByName(string name)
+        => Models.FirstOrDefault(x => x.Model == name);
+    }
+}
---
+++ OOP Exams/13. Retake Exam - 22 August 2020/01. Structure/Repositories/Entities/Repository.cs
@@ -0,0 +1,33 @@
+﻿using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public abstract class Repository<T> : IRepository<T>
+    {
+        private List<T> models;
+
+        public Repository()
+        {
+            models = new List<T>();
+        }
+
+        public ICollection<T> Models => models;
+
+        public void Add(T model)
+        {
+            models.Add(model);
+        }
+
+        public IReadOnlyCollection<T> GetAll()
+        => models.AsReadOnly();
+
+        public abstract T GetByName(string name);
+
+        public bool Remove(T model)
+        => models.Remove(model);
+    }
+}
