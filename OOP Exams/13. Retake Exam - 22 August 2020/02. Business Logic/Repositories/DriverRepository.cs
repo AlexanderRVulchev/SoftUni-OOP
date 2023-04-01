dbe7e673dfacd67c1e60bd959aed053dd67882ad
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-04-01 10:00:53 +0300

    Add files via upload

---
+++ OOP Exams/13. Retake Exam - 22 August 2020/02. Business Logic/Repositories/DriverRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Repositories
+{
+    using EasterRaces.Models.Drivers.Contracts;
+    using EasterRaces.Models.Drivers.Entities;
+    using EasterRaces.Repositories.Entities;
+    using System.Linq;
+
+    public class DriverRepository : Repository<IDriver>
+    {
+        public override IDriver GetByName(string name)
+        => Models.FirstOrDefault(x => x.Name == name);
+    }
+}
---
+++ OOP Exams/13. Retake Exam - 22 August 2020/02. Business Logic/Repositories/RaceRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Repositories
+{
+    using EasterRaces.Models.Races.Contracts;
+    using Models.Races.Entities;
+    using Repositories.Entities;
+    using System.Linq;
+
+    public class RaceRepository : Repository<IRace>
+    {
+        public override IRace GetByName(string name)
+        => Models.FirstOrDefault(x => x.Name == name);
+    }
+}
